4a865f487e1d5ac6c5ac6d6eb0a1df08202d5f9a
Patricia Murotani <patricia.murotani@gmail.com>
2022-08-07 16:49:30 -0300

    Desafio resolvido

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -4,15 +4,15 @@ public class ContaTerminal {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Digite o número de sua conta: ");
-        int conta = scanner.nextInt();
+        System.out.println("Digite o seu nome: ");
+        String nomeCliente = scanner.next();
 
         System.out.println("Digite o número de sua agência: ");
         String agencia = scanner.next();
 
-        System.out.println("Digite o seu nome: ");
-        String nomeCliente = scanner.next();
-
+        System.out.println("Digite o número de sua conta: ");
+        int conta = scanner.nextInt();
+        
         System.out.println("Digite seu saldo: ");
         double saldoCliente = scanner.nextDouble();
 
